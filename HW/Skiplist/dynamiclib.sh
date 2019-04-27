56d4c699c86f2ce86347f2021676c8e8145ba553
zz <zhaodonghao0105@163.com>
2019-04-27 15:31:51 +0800

    delete hw

--- HW/Skiplist/dynamiclib.sh
+++
@@ -1,3 +0,0 @@
-alias gcc = 'gcc -Wpedantic -Wall -Wextra -Werror --std=c89'
-gcc -shared -fPIC -c skiplist.c -o liblist.so
-gcc -o dynamiclist test.c -L. -llist
--- HW/TSP with Pthreads/tsp.h
+++
@@ -1,8 +0,0 @@
-#ifndef __tsp_h__		/*define*/
-#define __tsp_h__
-
-int get_next_route();
-int get_total_weight();
-void* exec();
-
-#endif                  /*end define*/
